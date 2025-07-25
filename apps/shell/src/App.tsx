a6bb070503966f8c54ea8b8313797bfa06282e55
jimmybastos <eujimmbastos@gmail.com>
2025-07-25 01:07:31 -0300

    feat: add error boundary to shell

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -4,6 +4,7 @@ import { Footer } from './components/app/footer';
 import { routes } from './routes';
 import { Toaster } from '@flagster/ui';
 import { ThemeProvider } from './hooks/use-theme';
+import { ErrorBoundary } from './components/app/error-boundary';
 
 function App() {
   return (
@@ -12,15 +13,17 @@ function App() {
         <BrowserRouter>
           <Navigation />
           <main className="container mx-auto py-8 px-4 pt-20">
-            <Routes>
-              {routes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.element}
-                />
-              ))}
-            </Routes>
+            <ErrorBoundary>
+              <Routes>
+                {routes.map((route) => (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={route.element}
+                  />
+                ))}
+              </Routes>
+            </ErrorBoundary>
           </main>
           <Footer />
         </BrowserRouter>
